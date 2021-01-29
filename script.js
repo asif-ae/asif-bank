52e863c35bbba62ded203b09a942e8be49fd3e1e
Asif Estiak <me.asif.ae@gmail.com>
2021-01-29 21:50:28 +0600

    this project will done letter

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+// Login button event handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener('click', function() {
-  console.log('clicked me');
+  document.getElementById('login-area').style.display = 'none';
+  document.getElementById('transaction-area').style.display = 'block';
 });
+
+// Deposit button event handler
+const depositBtn = document.getElementById('add-deposit');
+let depositAmount = document.getElementById('deposit-amount').value;
+depositBtn.addEventListener('click', function() {
+  let depositAmountStr = parseFloat(depositAmount);
+
+  document.getElementById('deposit-amount').value = "";
+
+  updateSpanText('current-deposit', depositAmountStr);
+
+  // Update current balance
+  updateSpanText('current-balance', tot);
+});
+
+function updateSpanText(id, plus) {
+  const str = document.getElementById(id).innerText;
+  const depo = parseFloat(str);
+  var tot = depo + plus;
+  const totF = tot.toFixed(2);
+  document.getElementById(id).innerText = totF;
+}
